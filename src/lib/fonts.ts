493517dd2adfdfa51e1cc5521b89c52671b86dd8
Elric <elric@bensonhomesolutions.com>
2026-08-16 23:16:35 -0700

    style: implement Benson brand typography

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,8 +1,22 @@
-import { Source_Sans_3 } from 'next/font/google'
+import { Libre_Baskerville, Montserrat, Source_Sans_3 } from 'next/font/google';
+
+export const libreBaskerville = Libre_Baskerville({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-libre-baskerville',
+  display: 'swap',
+});
+
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['600', '700'],
+  variable: '--font-montserrat',
+  display: 'swap',
+});
 
 export const sourceSans3 = Source_Sans_3({
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800', '900'],
+  weight: ['400', '600', '700'],
   variable: '--font-source-sans-3',
   display: 'swap',
-})
+});
